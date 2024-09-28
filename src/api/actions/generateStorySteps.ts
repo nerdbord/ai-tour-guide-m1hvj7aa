5fff1edd14147bffbae8b19ebde0ec53c09c0a00
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-09-28 22:29:50 +0200

    feat: generate story steps

---
+++ src/api/actions/generateStorySteps.ts
@@ -0,0 +1,10 @@
+﻿"use server";
+
+import { generateStorySteps } from "@/services/gpt.service";
+
+export const generateStoryStepsAction = async (
+  text: string,
+  keyConcepts: string[],
+) => {
+  return await generateStorySteps(text, keyConcepts);
+};
--- src/services/gpt.service.ts
+++ src/services/gpt.service.ts
@@ -2,20 +2,38 @@
 import { openai } from "../../openai.config";
 import { z } from "zod";
 
-const storyContextSchema = z.object({
+const StoryContextSchema = z.object({
   extractedText: z.string(),
   keyConcepts: z.array(z.string()),
 });
 
-export const convertImgToText = async (prompt: string, imgUrls: string[]) => {
+const NarrationSchema = z.object({
+  type: z.literal("narration"),
+  content: z.string(),
+});
+
+const DecisionSchema = z.object({
+  type: z.literal("decision"),
+  question: z.string(),
+  options: z.array(z.string()),
+});
+
+const StoryStepSchema = z.object({
+  storySteps: z.array(z.union([NarrationSchema, DecisionSchema])),
+});
+
+export const convertImgToText = async (imgUrls: string[]) => {
+  const PROMPT =
+    "Jesteś wirtualnym asystentem ucznia i otrzymasz zdjęcia materiałów do nauki. Zdjęcia będą pochodziły z podręcznika, z notatek, ze slajdów lub z ćwiczeń. Twoim zadaniem będzie wyciągniecie tego tekstu i zapisanie go w postaci string pod właściwością {extractedText} oraz kluczowych zagadnień z tego fragmentu, których uczeń powinien się nauczyć w formie pytań (pamiętaj żeby tak ułożyć pytania, na które odpowiedź jest zawarta w tekscie fragmentu) i zapisanie ich w postaci tablicy stringów pod właściwością {keyConcepts}. Zwróć tylko te dane, nic więcej.";
+
   const resp = await generateObject({
     model: openai("gpt-4o"),
-    schema: storyContextSchema,
+    schema: StoryContextSchema,
     messages: [
       {
         role: "user",
         content: [
-          { type: "text", text: prompt },
+          { type: "text", text: PROMPT },
           ...imgUrls.map(u => ({ type: "image" as const, image: u })),
         ],
       },
@@ -23,4 +41,22 @@ export const convertImgToText = async (prompt: string, imgUrls: string[]) => {
   });
 
   return resp.object;
+};
+
+export const generateStorySteps = async (
+  text: string,
+  keyConcepts: string[],
+) => {
+  const str = keyConcepts.join("");
+
+  const PROMPT = `Jesteś asystentem ucznia pomagającym w nauce poprzez tworzenie ciekawych historii na bazie przekazanych materiałów. Materiały zawierają tekst z podręcznika ${text} oraz kilka zagadnień ${str} które uczeń ma przyswoić. Zbuduj na podstawie tego tekstu historię, dzieląc ją na etapy narracyjne {type: "narration"} oraz elementy interaktywny, które będą polegały na podjęciu decyzji przez ucznia {type: "decision"}. Zwróć tylko etapy do pierwszego etapu typu "decision". Etap typu "decision" powinien pojawić się od razu po fragmencie, który zawiera odpowiedź na pytanie z etapu decyzyjnego.`;
+
+  const resp = await generateObject({
+    model: openai("gpt-4o"),
+    schema: StoryStepSchema,
+    prompt: PROMPT,
+  });
+
+  console.log("Generated story steps:", resp.object.storySteps);
+  return resp.object.storySteps;
 };
