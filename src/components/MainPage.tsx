6bfc27d1be0c2368290e951075e244b020e1f9f9
AniaPiWo <snowflake@wp.pl>
2024-09-28 17:03:56 +0200

    feat: main ui

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
+import { Button } from "./ui/Button";
+
+type Props = {};
+
+export const MainPage = (props: Props) => {
+  return (
+    <div>
+      <Button color="white">Moje historie</Button>
+      <h1 className="text-3xl not-italic font-bold mt-[78px]">
+        Czego chcesz się nauczyć?
+      </h1>
+      <p className="text-lg not-italic font-medium leading-6 mt-[26px]">
+        Wrzuć materiał, z którego mam <br /> stworzyć historię:
+      </p>
+      <div className="w-full h-40 border border-dashed flex flex-col items-center justify-center gap-6 mt-[26px] pt-7 pb-6 px-12">
+        <p className="text-center text-sm not-italic font-semibold leading-4">
+          Umieść obrazy materiałów, których chciałbyś się nauczyć.
+        </p>
+        <Button color="black">Wrzuć zdjęcia</Button>
+      </div>
+    </div>
+  );
+};
