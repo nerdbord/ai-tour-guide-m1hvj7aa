955a004d39b003923914ae3b0923df46d7711de1
AniaPiWo <snowflake@wp.pl>
2024-09-29 00:30:56 +0200

    styling hotfix

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 //import { StoryPage } from "@/components/StoryPage";
 //import { LoadingScreen } from "@/components/LoadingScreen";
-import { ChooseVoices } from "@/components/ChooseVoice";
+//import { ChooseVoices } from "@/components/ChooseVoice";
+import { Story } from "@/components/Story";
 
 const page = () => {
   /* return <StoryPage />; */
   /*  return <LoadingScreen />; */
-  return <ChooseVoices />;
+  /*   return <ChooseVoices />; */
+  return <Story />;
 };
 
 export default page;
