a9f2811fe022927c5a57e8bbcc7d65614dd34fa9
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-09-28 15:34:40 +0200

    feat: generate text from img

---
+++ src/api/actions/generateTextFromImage.ts
@@ -0,0 +1,12 @@
+﻿"use server";
+
+import { convertImgToText } from "@/services/gpt.service";
+
+export const generateTextFromImage = async (imageURL: string) => {
+  const prompt =
+    "Jesteś wirtualnym asystentem, który otrzyma zdjęcie tekst. Twoim zadaniem będzie wyciągniecie i zwrócenie tego tekstu jako string. Zwróć tylko tekst ze zdjęcia. Nie dodawaj dodatkowych opisów i informacji.";
+
+  const resp = await convertImgToText(prompt, imageURL);
+
+  return resp;
+};
---
+++ src/services/gpt.service.ts
@@ -0,0 +1,20 @@
+﻿import { generateText } from "ai";
+
+import { openai } from "../../openai.config";
+
+export const convertImgToText = async (prompt: string, imgUrl: string) => {
+  const resp = await generateText({
+    model: openai("gpt-4o"),
+    messages: [
+      {
+        role: "user",
+        content: [
+          { type: "text", text: prompt },
+          { type: "image", image: imgUrl },
+        ],
+      },
+    ],
+  });
+
+  return resp.text;
+};
