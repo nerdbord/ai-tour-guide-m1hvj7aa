e3fc4668015829a8c448b6ccb0930eed1dc80582
AniaPiWo <snowflake@wp.pl>
2024-09-28 23:43:57 +0200

    feat: voices

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 //import { StoryPage } from "@/components/StoryPage";
-import { LoadingScreen } from "@/components/LoadingScreen";
-//import { Voices } from "@/components/ui/Voices";
+//import { LoadingScreen } from "@/components/LoadingScreen";
+import { ChooseVoices } from "@/components/ChooseVoice";
 
 const page = () => {
   /* return <StoryPage />; */
-  return <LoadingScreen />;
-  /*   return <Voices />; */
+  /*  return <LoadingScreen />; */
+  return <ChooseVoices />;
 };
 
 export default page;
--- src/components/ui/Voices.tsx
+++ src/components/ui/Voices.tsx
@@ -1,29 +1,25 @@
-/* eslint-disable @typescript-eslint/no-empty-object-type */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import React from "react";
-import { Button } from "./Button";
+import { FaCheckCircle } from "react-icons/fa";
 
-type Props = {};
+interface VoiceProps {
+  name: string;
+  isSelected: boolean;
+  onClick: () => void;
+}
 
-export const Voices = (props: Props) => {
+export const Voices: React.FC<VoiceProps> = ({ name, isSelected, onClick }) => {
   return (
-    <div className="flex flex-col justify-between w-full h-full px-4">
-      <div>
-        <h1 className="text-3xl not-italic font-bold mt-20">Wybierz głos</h1>
-        <p className="text-lg not-italic font-medium leading-6 py-4">
-          Tym głosem będzie czytana <br /> Twoja opowieść
-        </p>
-      </div>
-
-      <div className="flex-grow flex items-center justify-center w-full">
-        <p className="text-center">Gotujemy dla ciebie opowieść...</p>
-      </div>
-
-      {/* Dodanie klasy w-auto do przycisku */}
-      <div className="mb-4 flex justify-center w-full">
-        <Button className="w-auto">Zaczynamy!</Button>
-      </div>
+    <div
+      className="relative border p-4 rounded second-bg h-32 cursor-pointer" // Usunięty nadmiarowy nawias
+      onClick={onClick}
+    >
+      <h2 className="text-lg font-semibold">{name}</h2>
+      {isSelected && (
+        <div className="absolute top-2 right-2">
+          <FaCheckCircle size={24} />
+        </div>
+      )}
     </div>
   );
 };
