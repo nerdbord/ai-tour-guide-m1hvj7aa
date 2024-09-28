d1ea6eeb46db6a967e6d639b07308da79564a790
AniaPiWo <snowflake@wp.pl>
2024-09-28 19:13:22 +0200

    cards, cards array

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,14 +1,27 @@
 "use client";
 import React from "react";
+import { FaCheckCircle } from "react-icons/fa";
 
 interface CardProps {
   title: string;
+  isSelected: boolean;
+  onClick: () => void;
 }
 
-export const Card: React.FC<CardProps> = ({ title }) => {
+export const Card: React.FC<CardProps> = ({ title, isSelected, onClick }) => {
   return (
-    <div className="border p-4 bg-white h-32">
+    <div
+      className={`relative border p-4 bg-white h-32 cursor-pointer ${
+        isSelected ? "border-green-500" : ""
+      }`}
+      onClick={onClick}
+    >
       <h2 className="text-lg font-semibold">{title}</h2>
+      {isSelected && (
+        <div className="absolute top-2 right-2 text-green-500">
+          <FaCheckCircle size={24} />
+        </div>
+      )}
     </div>
   );
 };
